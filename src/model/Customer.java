3551d5eef055ae1e504fd53186627968ee1da6c0
yakovalenko <jaroslawkovalenko@jmail.com>
2016-12-04 15:20:10 +0200

    customer

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -1,7 +1,29 @@
 package model;
 
-/**
- * Created by yakovalenko on 04.12.2016.
- */
 public class Customer {
+
+    private Integer customer_id;
+    private String customer_name;
+
+    public Customer(Integer customer_id, String customer_name) {
+        this.customer_id = customer_id;
+        this.customer_name = customer_name;
+    }
+
+    public Integer getCustomer_id() {
+        return customer_id;
+    }
+
+    public void setCustomer_id(Integer customer_id) {
+        this.customer_id = customer_id;
+    }
+
+    public String getCustomer_name() {
+        return customer_name;
+    }
+
+    public void setCustomer_name(String customer_name) {
+        this.customer_name = customer_name;
+    }
 }
+
