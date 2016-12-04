c2ac8efab44f4ab5af574fa3ff2b47ff76c4dc4c
yakovalenko <jaroslawkovalenko@jmail.com>
2016-12-04 19:09:05 +0200

    add daocompanies & daoskills interfaces

---
+++ src/model/dao/DaoSkills.java
@@ -0,0 +1,27 @@
+package model;
+
+import java.util.List;
+
+public class DaoSkills implements GenericDao<Skill> {
+
+
+    @Override
+    public Skill create(Skill skill) {
+        return null;
+    }
+
+    @Override
+    public List<Skill> get(long id) {
+        return null;
+    }
+
+    @Override
+    public void update(Skill skill) {
+
+    }
+
+    @Override
+    public void delete(long id) {
+
+    }
+}
