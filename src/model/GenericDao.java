071918491846abff88127dab7646c538ca09afb9
VladRybkin <Footballer0@mail.ru>
2016-12-04 17:06:43 +0200

    GenericDAO

--- src/model/GenericDao.java
+++ src/model/GenericDao.java
@@ -1,12 +1,14 @@
 package model;
 
+import java.util.List;
+
 /**
  * Created by Vlad on 04.12.2016.
  */
 public interface GenericDao <T>{
     T create(T t);
-    T get(long id);
-    T update(T t);
-    T delete(long id);
+    List<T> get(long id);
+    void update(T t);
+    void delete(long id);
 
 }
