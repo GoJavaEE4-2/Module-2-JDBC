7963ef73d5168b4c0820d411e0b974c0d7706100
VladRybkin <Footballer0@mail.ru>
2016-12-04 19:28:00 +0200

    Merge branch 'master' of https://github.com/GoJavaEE4-2/Module-2-JDBC
    # Conflicts:
    #	src/model/entities/Customer.java

--- src/model/dao/DAO.java
+++ src/model/dao/DAO.java
@@ -7,8 +7,8 @@ import java.util.List;
  * Created by Vlad on 04.12.2016.
  */
 public interface DAO<T>{
-    T create(T t);
-    List<T> get(long id);
+    void create(T t);
+    void get(long id);
     void update(T t);
     void delete(long id);
 
