a8b51e054b4f1b43dbcfb0e59e692e4103b61cd4
yakovalenko <jaroslawkovalenko@jmail.com>
2016-12-05 23:20:15 +0200

    Developer  fix

--- src/model/entities/Developer.java
+++ src/model/entities/Developer.java
@@ -5,70 +5,70 @@ import java.util.Date;
  */
 public class Developer {
 
-    public Developer(int developer_id, String developer_name, int developer_company_id, int developer_project_id, Date developer_join_date) {
-        this.developer_id = developer_id;
-        this.developer_name = developer_name;
-        this.developer_company_id = developer_company_id;
-        this.developer_project_id = developer_project_id;
-        this.developer_join_date = developer_join_date;
+    public Developer(int developerId, String developerName, int developerCompanyId, int developerProjectId, Date developerJoinDate) {
+        this.developerId = developerId;
+        this.developerName = developerName;
+        this.developerCompanyId = developerCompanyId;
+        this.developerProjectId = developerProjectId;
+        this.developerJoinDate = developerJoinDate;
     }
 
-    public int getDeveloper_id() {
-        return developer_id;
+    public int getDeveloperId() {
+        return developerId;
     }
 
-    public void setDeveloper_id(int developer_id) {
-        this.developer_id = developer_id;
+    public void setDeveloperId(int developerId) {
+        this.developerId = developerId;
     }
 
-    public String getDeveloper_name() {
-        return developer_name;
+    public String getDeveloperName() {
+        return developerName;
     }
 
-    public void setDeveloper_name(String developer_name) {
-        this.developer_name = developer_name;
+    public void setDeveloperName(String developerName) {
+        this.developerName = developerName;
     }
 
-    public int getDeveloper_company_id() {
-        return developer_company_id;
+    public int getDeveloperCompanyId() {
+        return developerCompanyId;
     }
 
-    public void setDeveloper_company_id(int developer_company_id) {
-        this.developer_company_id = developer_company_id;
+    public void setDeveloperCompanyId(int developerCompanyId) {
+        this.developerCompanyId = developerCompanyId;
     }
 
-    public int getDeveloper_project_id() {
-        return developer_project_id;
+    public int getDeveloperProjectId() {
+        return developerProjectId;
     }
 
-    public void setDeveloper_project_id(int developer_project_id) {
-        this.developer_project_id = developer_project_id;
+    public void setDeveloperProjectId(int developerProjectId) {
+        this.developerProjectId = developerProjectId;
     }
 
-    public Date getDeveloper_join_date() {
-        return developer_join_date;
+    public Date getDeveloperJoinDate() {
+        return developerJoinDate;
     }
 
-    public void setDeveloper_join_date(Date developer_join_date) {
-        this.developer_join_date = developer_join_date;
+    public void setDeveloperJoinDate(Date developerJoinDate) {
+        this.developerJoinDate = developerJoinDate;
     }
 
     @Override
     public String toString() {
         return "Developer{" +
-                "developer_id=" + developer_id +
-                ", developer_name='" + developer_name + '\'' +
-                ", developer_company_id=" + developer_company_id +
-                ", developer_project_id=" + developer_project_id +
-                ", developer_join_date=" + developer_join_date +
+                "developerId=" + developerId +
+                ", developerName='" + developerName + '\'' +
+                ", developerCompanyId=" + developerCompanyId +
+                ", developerProjectId=" + developerProjectId +
+                ", developerJoinDate=" + developerJoinDate +
                 '}';
     }
 
-    private int developer_id ;
-    private String developer_name;
-    private int  developer_company_id;
-    private int developer_project_id;
-    private Date developer_join_date;
+    private int developerId ;
+    private String developerName;
+    private int  developerCompanyId;
+    private int developerProjectId;
+    private Date developerJoinDate;
 
 
 }
