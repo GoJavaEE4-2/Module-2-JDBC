55ae52359588b3f87bd7ae71bc33afa340dfe820
Andrey Sevruk <sevrukandrey@gmail.com>
2016-12-06 11:51:42 +0200

    change type of variable

--- src/model/entities/Project.java
+++ src/model/entities/Project.java
@@ -4,57 +4,78 @@ import java.util.Date;
 
 public class Project {
 
-    private int projectID;
-    private String projectName;
-    private int projectCompanyId;
-    private int projectCustomerId;
-    private Date projectTimeStamp;
+    private int project_id ;
+    private String project_name;
+    private Company project_company_id;
+    private Customer project_customer_id;
+    private Date project_start_timestamp;
+    private double project_cost;
 
-    public Project(int projectID, String projectName, int projectCompanyId, int projectCustomerId, Date projectTimeStamp) {
-        this.projectID = projectID;
-        this.projectName = projectName;
-        this.projectCompanyId = projectCompanyId;
-        this.projectCustomerId = projectCustomerId;
-        this.projectTimeStamp = projectTimeStamp;
+    public int getProject_id() {
+        return project_id;
     }
 
-    public int getProjectID() {
-        return projectID;
+    public void setProject_id(int project_id) {
+        this.project_id = project_id;
     }
 
-    public void setProjectID(int projectID) {
-        this.projectID = projectID;
+    public String getProject_name() {
+        return project_name;
     }
 
-    public String getProjectName() {
-        return projectName;
+    public void setProject_name(String project_name) {
+        this.project_name = project_name;
     }
 
-    public void setProjectName(String projectName) {
-        this.projectName = projectName;
+    public Company getProject_company_id() {
+        return project_company_id;
     }
 
-    public int getProjectCompanyId() {
-        return projectCompanyId;
+    public void setProject_company_id(Company project_company_id) {
+        this.project_company_id = project_company_id;
     }
 
-    public void setProjectCompanyId(int projectCompanyId) {
-        this.projectCompanyId = projectCompanyId;
+    public Customer getProject_customer_id() {
+        return project_customer_id;
     }
 
-    public int getProjectCustomerId() {
-        return projectCustomerId;
+    public void setProject_customer_id(Customer project_customer_id) {
+        this.project_customer_id = project_customer_id;
     }
 
-    public void setProjectCustomerId(int projectCustomerId) {
-        this.projectCustomerId = projectCustomerId;
+    public Date getProject_start_timestamp() {
+        return project_start_timestamp;
     }
 
-    public Date getProjectTimeStamp() {
-        return projectTimeStamp;
+    public void setProject_start_timestamp(Date project_start_timestamp) {
+        this.project_start_timestamp = project_start_timestamp;
     }
 
-    public void setProjectTimeStamp(Date projectTimeStamp) {
-        this.projectTimeStamp = projectTimeStamp;
+    public double getProject_cost() {
+        return project_cost;
     }
+
+    public void setProject_cost(double project_cost) {
+        this.project_cost = project_cost;
+    }
+
+    @Override
+    public String toString() {
+        return "Project{" +
+                "project_id=" + project_id +
+                ", project_name='" + project_name + '\'' +
+                ", project_company_id=" + project_company_id +
+                ", project_customer_id=" + project_customer_id +
+                ", project_start_timestamp=" + project_start_timestamp +
+                ", project_cost=" + project_cost +
+                '}';
+    }
+
+
+
+
+
+
+
+
 }
