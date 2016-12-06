37cd790e1c018269f26a144bd3f6ee7af0588691
yakovalenko <jaroslawkovalenko@jmail.com>
2016-12-06 10:18:40 +0200

    adding data for connection to DB

--- src/model/dao/SkillDAOImpl.java
+++ src/model/dao/SkillDAOImpl.java
@@ -1,10 +1,17 @@
 package model.dao;
 
-
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.List;
 
 public class SkillDAOImpl<Skill> implements SkillDAO<Skill> {
 
+    public static final String JDBC_DRIVER = "org.postgresql.Driver";
+    public static final String DATABASE_URL = "jdbc:postgresql://localhost:5432/postgres";
+    public static Connection connection = null;
+    public static Statement statement = null;
+    public static PreparedStatement preparedStatement = null;
 
 
 
