64dfd712bf83de9d6c567b9288885ba2e88b3758
yakovalenko <jaroslawkovalenko@jmail.com>
2016-12-08 16:55:47 +0200

    connection util

--- src/utilities/ConnectionUtils.java
+++ src/utilities/ConnectionUtils.java
@@ -1,7 +1,24 @@
 package utilities;
 
+import java.io.*;
+import java.util.Properties;
+
 public class ConnectionUtils {
     public static void main(String[] args) {
+
+        FileInputStream fileInputStream;
+        Properties property = new Properties();
+        String path = "config.properties";
+
+            try {
+                fileInputStream = new FileInputStream(path);
+                property.load(fileInputStream);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
 
     }
 }
