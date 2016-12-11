d50caaef3a032f801249b42df46bee0433d5e0b6
yakovalenko <jaroslawkovalenko@jmail.com>
2016-12-11 23:06:26 +0200

    controller for skills

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,10 +1,53 @@
 package controller;
 
+import model.dao.CustomersDAOImpl;
+import model.dao.DevelopersDAOImpl;
+import model.dao.ProjectDAOImpl;
 import model.dao.SkillDAOImpl;
+import model.entities.Skill;
+import view.ConsoleDataInput;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Controller {
 
-    SkillDAOImpl skillDAOImpl = new SkillDAOImpl();
+    SkillDAOImpl skillDAOImpl;
+    
+    public Controller(SkillDAOImpl skillDAOImpl, ProjectDAOImpl projectDAOImpl, DevelopersDAOImpl developersDAOImpl, CustomersDAOImpl customersDAOImpl) {
+        this.skillDAOImpl = skillDAOImpl;
+    }
 
+    public void createSkillDB() throws IOException {
+        Skill skill = new Skill(ConsoleDataInput.readString());
+        skillDAOImpl.create(skill);
+    }
+
+
+    public void updateSkillDB() throws IOException {
+        Skill skill = new Skill(ConsoleDataInput.readString());
+        skillDAOImpl.update(skill);
+    }
+
+    public Skill getSkillDB() throws IOException {
+        Skill skill = skillDAOImpl.get(ConsoleDataInput.readInt());
+        return skill;
+    }
+
+    public void deleteSkillFromDb() throws IOException {
+        skillDAOImpl.delete(ConsoleDataInput.readInt());
+    }
+
+    public Skill findByNameSkillDB() throws IOException {
+        Skill skill = new Skill(skillDAOImpl.findByName(ConsoleDataInput.readString()));
+        return skill;
+    }
+
+    public List<Skill> getAllSkillsFromDb() throws IOException {
+        List<Skill> skills = new ArrayList<>();
+        return skills = skillDAOImpl.getAll();
+    }
 
 }
--- src/view/View.java
+++
@@ -1,9 +0,0 @@
-package view;
-
-
-public class View {
-
-    public void printSkillName(String name){
-        System.out.println("Skill name is:" + name);
-    }
-}
